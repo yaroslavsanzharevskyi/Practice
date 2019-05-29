603709d254071e66ce30d85b4f8758c41d80fc1e
Yaroslav Sanzharevskyi <ysanz@softserveinc.com>
2019-05-29 12:06:08 +0300

    Add two sum algorithms

---
+++ LeetCode/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using LeetCode.Algorithms;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TwoSumAlgorithm alg = new TwoSumAlgorithm();
+            alg.TwoSum(new[]{0,3,4,0 }, 0);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
