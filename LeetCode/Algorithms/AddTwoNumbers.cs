95d46a84f8c928655d2b45bf7f7a7617b51022fc
Yaroslav Sanzharevskyi <ysanz@softserveinc.com>
2019-07-23 13:59:54 +0300

    AddTwoNumbers: completed;

---
+++ LeetCode/Algorithms/AddTwoNumbers.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace LeetCode.Algorithms
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+    public class AddTwoNumbers
+    {
+        public ListNode Execute(ListNode l1, ListNode l2)
+        {
+            return RecursiveExecute(l1, l2, 0);
+        }
+
+        private ListNode RecursiveExecute(ListNode l1, ListNode l2, int addToSum)
+        {
+            if (l1 == null && l2 == null)
+            {
+                return null;
+            }
+
+            var l1val = 0;
+            var l2val = 0;
+
+            if (l1 != null)
+            {
+                l1val = l1.val;
+            }
+
+            if (l2 != null)
+            {
+                l2val = l2.val;
+            }
+
+            var sum = l1val + l2val + addToSum;
+            var currentValue = 0;
+            var toNextLevel = 0;
+            if (sum < 10)
+            {
+                currentValue = sum;
+            }  else if (sum == 10)
+            {
+                toNextLevel = 1;
+            }
+            else if(sum > 10)
+            {
+                currentValue = sum % 10;
+                toNextLevel = 1;
+            }
+
+            var node = new ListNode(currentValue);
+            node.next = RecursiveExecute(l1?.next, l2?.next, toNextLevel);
+            if (node.next == null && toNextLevel != 0)
+            {
+                node.next = new ListNode(toNextLevel);
+            }
+
+            return node;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -3,13 +3,18 @@ using LeetCode.Algorithms;
 
 namespace LeetCode
 {
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            TwoSumAlgorithm alg = new TwoSumAlgorithm();
-            alg.TwoSum(new[]{0,3,4,0 }, 0);
-            Console.WriteLine("Hello World!");
+            
+            Console.WriteLine(12%10);
+            Console.WriteLine(9 % 10);
+            Console.WriteLine(10 % 10);
+            Console.WriteLine(0 % 10);
+
         }
     }
 }
