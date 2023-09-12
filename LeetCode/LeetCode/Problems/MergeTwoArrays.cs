86e8ccdd7d23e029571e545c24f549da778a1c2e
Yaroslav Sanzharevskyi <yaroslav.sanzharevskyi@hatch.com>
2023-09-11 21:24:29 -0400

    Add solution for MergeTwoArrays

---
+++ LeetCode/LeetCode/Problems/MergeTwoArrays.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace LeetCode.Problems;
+public class MergeTwoArrays
+{
+   public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        for (var imn = m + n; imn > m; imn--)
+        {
+            if (m > 0 && nums1[m - 1] > nums2[n - 1])
+            {
+                nums1[imn-1] = nums1[--m];
+            }
+            else
+            {
+                nums1[imn-1] = nums2[--n];
+            }
+        }
+    }
+}
