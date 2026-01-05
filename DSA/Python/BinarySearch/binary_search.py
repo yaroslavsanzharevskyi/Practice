5f9e25933bab59c2f57e8a2b213344fc79a5d393
Yaroslav Sanzharevskyi <sanjarevsky.yaroslav@gmail.com>
2026-01-05 10:43:26 -0500

    Refactor binary search implementation to include complete function definition and ensure proper loop condition

--- DSA/Python/BinarySearch/binary_search.py
+++ DSA/Python/BinarySearch/binary_search.py
@@ -1,2 +1,17 @@
 def search(nums: List[int], target: int) -> int:
+    left = 0
+    right = len(nums) - 1
     
+    while left < right:
+        pointer = left + (right-left)//2 #int((right-left)/2)
+        
+        value = nums[pointer]
+        if value == target:
+            return pointer
+        
+        if value < target:
+            left = pointer + 1
+        else:
+            right = pointer - 1
+            
+    return -1
